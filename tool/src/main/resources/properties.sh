785a1d5aebe2b0e61e95fd50d47e6f878bbc82e0
Anna_Babich <Anna_Babich@epam.com>
2014-09-17 14:54:11 +0300

    change properties file

--- tool/src/main/resources/properties.sh
+++ tool/src/main/resources/properties.sh
@@ -1,8 +1,8 @@
 # hosts on which GSM are located, e.g. ("10.6.133.203" "10.6.133.210")
-GSM_HOSTS=("10.6.132.93")
+GSM_HOSTS=("10.6.133.210")
 
-# map with key value pairs, where key is processing unit name, value is name of file with new classes
-declare -A PU=( [space]=space.jar [cinema]=cinema.war )
+# map with key value pairs, where key is processing unit name, value is name of file with new classes, e.g. declare -A PU=( [space]=space.jar )
+declare -A PU=( [space]=space.jar )
 
 # name of user on remote machine, e.g. "user"
 SSS_USER="user"
@@ -11,16 +11,16 @@ SSS_USER="user"
 GIGASPACES_LOCATION="/home/user/gigaspaces-xap-premium-10.0.0-ga/"
 
 # gigaspace locator to cluster, e.g. "127.0.0.1"
-GIGASPACES_LOCATORS="10.6.132.93"
+GIGASPACES_LOCATORS="127.0.0.1"
 
 # lookup group, e.g. "gigaspaces-10.0.0-XAPPremium-ga"
 LOOKUP_GROUP="gigaspaces-10.0.0-XAPPremium-ga"
 
 # timeout for identify PU (in seconds), e.g. "10"
-IDENT_PU_TIMEOUT="100"
+IDENT_PU_TIMEOUT="10"
 
 # timeout for identify space mode (in seconds), e.g. "10"
-IDENT_SPACE_MODE_TIMEOUT="100"
+IDENT_SPACE_MODE_TIMEOUT="10"
 
 # set "true" if space secured, set "false" if space not secured
 IS_SECURED="false"
